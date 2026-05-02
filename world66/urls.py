5f5daca8a4efd1f3874f3f54c7a5b6c7b6909c6f
Richard <richardosinga@gmail.com>
2026-05-02 20:54:36 +0200

    Wire spacetime_app into settings and urls

--- world66/urls.py
+++ world66/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from guide import views
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path("tags/<str:tag>", views.tag_index, name="tag_index"),
     path("content-image/<path:path>", views.content_image, name="content_image"),
     path("review", views.review, name="review"),
+    path("timespace/", include("spacetime_app.urls")),
     path("<path:path>", views.location_or_section, name="location_or_section"),
 ]

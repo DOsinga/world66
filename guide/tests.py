bdbd9b861817cab05fcb68f88ad68244c22a2760
Douwe Osinga <douwe.osinga@gmail.com>
2026-08-29 17:57:22 -0400

    Overhaul Bonaire guide

--- guide/tests.py
+++ guide/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class DestinationPoiAggregationTest(SimpleTestCase):
+    def test_opted_in_destination_section_lists_flat_pois(self):
+        response = self.client.get(
+            "/northamerica/netherlandsantilles/bonaire/things_to_do"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Bonaire National Marine Park")
+        self.assertContains(response, "1000 Steps")
+        self.assertContains(response, "Cadushy Distillery")
